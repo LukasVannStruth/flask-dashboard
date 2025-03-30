6db23491ae65c201291e7a63f3af6278b2629646
Lukas Vann Struth <lvannstruth@gmail.com>
2025-03-29 18:27:46 -0700

    added version to webpgae

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 import os
+from . import __version__
 
 app = Flask(__name__)
 
@@ -7,9 +8,17 @@ template_dir = os.path.join(os.getcwd(), 'templates')
 index_file = os.path.join(template_dir, 'index.html')
 
 
+envvars = {}
+
 
 @app.route("/")
 def render_index():
     stylefile_path = url_for('static', filename='styles.css')
-    return render_template('index.html', stylesheet=stylefile_path)
+
+
+    # define envvars for the page
+    envvars["stylesheet"] = stylefile_path 
+    envvars["version"] = __version__
+
+    return render_template('index.html', envvars=envvars)
     
